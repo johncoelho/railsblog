7d3848dbe1bf458151a39e907a409b32d7126806
John <joncqs@gmail.com>
2012-12-01 12:05:31 -0200

    Versao inicialgit add . by john

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9ee8511359709cc7fb745f11b7d97d574e6f5e927bc40e457c781b0e6b5b6f1a2466b534d6fe282310b85d150b2675ff9fbe8929a900652d3aeda1fa2a4944c9'
